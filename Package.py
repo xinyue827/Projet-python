2d7dc0b7255176803210aba94e2024421649ffdf
Jingjinn <158856640+Jingjinn@users.noreply.github.com>
2024-03-29 04:00:36 +0100

    Add files via upload

---
+++ Package.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Packages
+
+# In[ ]:
+
+
+import time
+import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+import os
+from openai import OpenAI
+import resend
+
